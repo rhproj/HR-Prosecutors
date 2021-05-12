d86318918cb754c8c4476c098224af95b0a3f0c9
Rh Proj <r.proj@hotmail.com>
2021-05-12 11:44:51 +0300

    HR manager (Console)

--- HRtest/Program.cs
+++ HRtest/Program.cs
@@ -10,6 +10,19 @@ namespace HRtest
     {
         static void Main(string[] args)
         {
+            var dbContext = new K092DBContext();
+
+            var activeStaff = dbContext.CADRE_VIEW_PERSONSL.ToList().OrderBy(p=>p.FIO);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int i = 1;
+            foreach (var person in activeStaff)
+            {
+                Console.WriteLine($"{i++}. {person.FIO} Подразделение: {person.POD}. Должность: {person.DOL}."); //({person.ISN_PERSON})
+            }
+
+            Console.ReadKey();
         }
     }
 }
