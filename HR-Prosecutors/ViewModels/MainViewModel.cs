730897097e1b7a63ba98c500b07823e362f9ccc9
Rh Proj <r.proj@hotmail.com>
2021-05-12 14:18:40 +0300

    HR_Prosecutors

---
+++ HR-Prosecutors/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Prosecutors.ViewModels
+{
+    class MainViewModel
+    {
+        private int _title = 7;
+
+        public int Title
+        {
+            get { return _title = 7; }
+            set { _title = value; }
+        }
+
+    }
+}
